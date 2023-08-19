576a754ab1080395e13f67da1302688b2203a178
Evan Hu <evanhu@berkeley.edu>
2023-08-18 20:07:10 -0700

    fixed link

---
+++ app/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <>
+      <div className="absolute mb-4 bottom-0 left-1/2 mx-auto -translate-x-1/2">
+        <a href="https://www.ocf.berkeley.edu">
+            <img src="http://www.ocf.berkeley.edu/hosting-logos/ocf-hosted-penguin-dark.svg"
+                alt="Hosted by the OCF" className="border-0" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 export const metadata: Metadata = {
   title: 'Generative AI at Berkeley',
@@ -13,10 +14,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className='font-sans bg-zinc-950'>
+    <html lang="en" className="h-fit">
+      <body className='font-sans bg-zinc-950 h-fit'>
         <NavBar></NavBar>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   )
